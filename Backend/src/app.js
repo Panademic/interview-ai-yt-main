0a55ea9fcb64f217eae20f44cb83b63f16a09e00
Panademic <shubhambhardwajwppe@gmail.com>
2026-06-25 22:17:27 +0530

    zxcc

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://interview-ai-yt-main-7qj99d9mu-panademics-projects.vercel.app/",
+    origin: "https://interview-ai-yt-main-two.vercel.app/",
     credentials: true
 }))
 
