13180f01ee61de29a46afd2845a095e3a69d180d
Panademic <shubhambhardwajwppe@gmail.com>
2026-06-25 22:19:34 +0530

    cors error fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://interview-ai-yt-main-two.vercel.app/",
+    origin: "https://interview-ai-yt-main-two.vercel.app",
     credentials: true
 }))
 
